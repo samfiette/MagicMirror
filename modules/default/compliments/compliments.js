bc12c9b5cf0f462442541e4a71a27ede061da01c
Sam <sam.fiette@etu.emse.fr>
2022-03-07 18:59:01 +0100

    First

--- modules/default/compliments/compliments.js
+++ modules/default/compliments/compliments.js
@@ -8,11 +8,7 @@ Module.register("compliments", {
 	// Module config defaults.
 	defaults: {
 		compliments: {
-			anytime: ["Hey there sexy!"],
-			morning: ["Good morning, handsome!", "Enjoy your day!", "How was your sleep?"],
-			afternoon: ["Hello, beauty!", "You look sexy!", "Looking good today!"],
-			evening: ["Wow, you look hot!", "You look nice!", "Hi, sexy!"],
-			"....-01-01": ["Happy new year!"]
+			anytime: ["Salut JP l'immense bg!"]
 		},
 		updateInterval: 30000,
 		remoteFile: null,
